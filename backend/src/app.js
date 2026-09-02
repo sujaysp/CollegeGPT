047d6eefaec14f992b0a114d259a82de969427a0
Sujay <sujaysp.28@gmail.com>
2026-09-02 11:12:41 +0530

    Allow production frontend in CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ const app = express();
  */
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
     credentials: true,
   })
 );
